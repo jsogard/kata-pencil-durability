f519ecd2d79cb43c690bcf907679383cc090687c
Joe <jmsogard@gmail.com>
2017-08-27 23:40:48 -0500

    implemented edit

--- src/pencil_durability/Paper.java
+++ src/pencil_durability/Paper.java
@@ -16,7 +16,7 @@ public class Paper {
 		int i = 0;
 		char c;
 		while(pencil.getPointDurability() > 0 && i < text.length()){
-			c = text.charAt(i);
+			c = text.charAt(i++);
 			if(pencil.erodePoint( CharacterCost.getWriteCost(c) ))
 				this.text += c;
 			else
@@ -29,8 +29,8 @@ public class Paper {
 		int lastIndex = this.text.lastIndexOf(text);
 		if(lastIndex == -1) return false;
 		
-		char[] cstring = text.toCharArray();
-		for(int i = lastIndex + text.length(); i >= lastIndex; i--){
+		char[] cstring = this.text.toCharArray();
+		for(int i = lastIndex + text.length()-1; i >= lastIndex; i--){
 			
 			if(pencil.erodeEraser( CharacterCost.getEraseCost(cstring[i]) ))
 				cstring[i] = ' ';
@@ -42,6 +42,39 @@ public class Paper {
 		
 		this.text = new String(cstring);
 		return true;
+	}
+	
+	public boolean edit(String text, int index, Pencil pencil){
+		char[] cstring = this.text.toCharArray();
+		String overflow = null;
+		int i = 0;
+		char write_c;
+		
+		if(index + text.length() > this.text.length())
+			overflow = text.substring(this.text.length() - index);
+		
+		while(pencil.getPointDurability() > 0 && index < this.text.length()){
+			
+			write_c = text.charAt(i);
+			if(pencil.erodePoint( CharacterCost.getWriteCost(write_c) )){
+				cstring[index] = getOverwriteChar(write_c, cstring[index]);
+			}
+			else
+				return false;
+			i++;
+			index++;
+		}
+		this.text = new String(cstring);
+		
+		if(overflow != null) return write(overflow, pencil);
+		return true;
+	}
+	
+	private char getOverwriteChar(char newChar, char oldChar){
+		if(Character.isWhitespace(newChar)) return oldChar;
+		if(Character.isWhitespace(oldChar)) return newChar;
+		if(newChar == oldChar) return oldChar;
+		return '@';
 	}
 	
 }
