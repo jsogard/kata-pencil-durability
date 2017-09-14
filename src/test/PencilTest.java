6ce44407768b48c9102e921e0e8d4379af932373
Joe Sogard <jmsogard@gmail.com>
2017-09-14 15:02:10 -0500

    separate main and test packages

---
+++ src/test/PencilTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import main.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PencilTest {
+
+	@Test
+	public void initialPointDurability(){
+		
+	}
+
+}
