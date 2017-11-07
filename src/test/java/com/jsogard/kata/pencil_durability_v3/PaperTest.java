e2a9092efb8b58a29d7e5867064fdb0b196c8dad
Joe Sogard <jmsogard@gmail.com>
2017-11-07 12:05:05 -0600

    gather requirements, comment test cases

--- src/test/java/com/jsogard/kata/pencil_durability_v3/PaperTest.java
+++ src/test/java/com/jsogard/kata/pencil_durability_v3/PaperTest.java
@@ -7,5 +7,64 @@ public class PaperTest extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
+	
+	/* 
+	 * TC00-WRITE_APPEND
+	 * Text written by the pencil should always be appended to existing text on the paper:
+	 * SCENARIO: 
+	 * 	given a piece of paper with the text "She sells sea shells", 
+	 * 	when a pencil is instructed to write "down by the sea shore" on the paper, 
+	 * 	the paper will then contain the entire string
+	 */
+	
+	
+	/* 
+	 * TC02-POINTDEG_DULL_WRITE
+	 * after it goes dull, every character it is directed to write will appear as a space
+	 * SCENARIO:
+	 * 	if a pencil with point durability of four is instructed to write the string "Text", 
+	 * 	the paper will only show "Tex "
+	 */
+	
+	/*
+	 * TC09-ERASE_LAST_OCCURANCE
+	 * When the pencil is instructed to erase text from the paper, the last occurrence of that text on the paper will be replaced with empty spaces
+	 * SCENARIO: 
+	 * 	Given a piece of the paper containing the string:
+	 * 	"How much wood would a woodchuck chuck if a woodchuck could chuck wood?"
+	 * 	when the string "chuck" is erased, the paper should read:
+	 * 	"How much wood would a woodchuck chuck if a woodchuck could       wood?"
+	 * 	and if the string "chuck" is erased again, the paper should read:
+	 * 	"How much wood would a woodchuck chuck if a wood       could       wood?"
+	 */
+	
+	/*
+	 * TC12-ERASERDEG_INSUFF_DURABILITY
+	 * Once the eraser durability is zero, the eraser is worn out and can no longer erase
+	 * SCENARIO:
+	 * 	Thus if a pencil's eraser has remaining durability of three, 
+	 * 	and it is instructed to erase the word "Bill" from "Buffalo Bill", 
+	 * 	then the text remaining on the paper is "Buffalo B   "
+	 */
+	
+	/*
+	 * TC13-EDIT_OVER_WHITESPACE
+	 * Once text has been erased from the paper, a pencil may be instructed to write new text over the resulting white space
+	 * SCENARIO:
+	 * 	if the paper contains the text "An       a day keeps the doctor away", 
+	 * 	a pencil can can be instructed to write the word "onion" in the white space gap, 
+	 * 	so the text reads "An onion a day keeps the doctor away".
+	 */
+	
+	/*
+	 * TC14-EDIT_COLLISION
+	 * If the new text is longer than the allocated whitespace 
+	 * and thus would collide with other existing non-whitespace characters on the page, 
+	 * these character collisions should be represented by the "@" character
+	 * SCENARIO:
+	 * 	writing "artichoke" 
+	 * 	in the middle of "An       a day keeps the doctor away" 
+	 * 	would result in "An artich@k@ay keeps the doctor away"
+	 */
 
 }
--- src/test/java/com/jsogard/kata/pencil_durability_v3/PencilTest.java
+++ src/test/java/com/jsogard/kata/pencil_durability_v3/PencilTest.java
@@ -7,5 +7,54 @@ public class PencilTest extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
+	
+	/* 
+	 * TC01-PENCIL_INIT
+	 * When a pencil is created, it can be provided with a value for point durability
+	 * A pencil should also be created with an initial length value.
+	 * When a pencil is created, it can be provided with a value for eraser durability
+	 */
+	
+	/*
+	 * TC03-POINTDEG_WHITESPACE_WRITE
+	 * Writing spaces and newlines expends no graphite; therefore these characters should not affect the pencil point
+	 */
+	
+	/*
+	 * TC04-POINTDEG_LOWERCASE_WRITE
+	 * Lowercase letters should degrade the pencil point by a value of one
+	 */
+	
+	/*
+	 * TC05-POINTDEG_UPPERCASE_WRITE
+	 * capital letters should degrade the point by two
+	 */
+	
+	/*
+	 * TC06-SHARPEN_POINT_DUR
+	 * When a pencil is sharpened, it regains its initial point durability
+	 */
+	
+	/*
+	 * TC07-SHARPEN_REDUCE_LENGTH
+	 * The pencil's length is reduced by one each time it is sharpened
+	 */
+	
+	/*
+	 * TC08-SHARPEN_NO_LENGTH
+	 * When a pencil's length is zero, then sharpening it no longer restores its point durabliity
+	 */
+	
+	/*
+	 * TC10-ERASERDEG_WHITESPACE
+	 * all characters except for white space should degrade the eraser by a value of one
+	 */
+	
+	/*
+	 * TC11-ERASERDEG_CHARACTER
+	 * all characters except for white space should degrade the eraser by a value of one
+	 */
+	
+	
 
 }
