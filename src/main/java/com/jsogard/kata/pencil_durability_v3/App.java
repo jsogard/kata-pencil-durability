13ef50d8f2efcd9d1f1c0228c1e7ca83830c2b6e
Joe Sogard <jmsogard@gmail.com>
2017-11-07 11:02:38 -0600

    first commit for rework

---
+++ src/main/java/com/jsogard/kata/pencil_durability_v3/App.java
@@ -0,0 +1,13 @@
+package com.jsogard.kata.pencil_durability_v3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
